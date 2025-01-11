322484088f23e8cb07a067b2a8d18c323071b745
Saturn-82 <laisalves.rocha@gmail.com>
2025-01-11 23:37:20 +0000

    Atualização de exercício

--- exercicios/hello-world.py
+++ exercicios/hello-world.py
@@ -1,4 +1,4 @@
 #Digite print('Olá, mundo!')
 print('Olá, mundo!')
-print('Gabe é um bobão, mas eu amo muito ele"')
-print('Gabe só me irrita mais ainda sim ele é meu amorzinho é so euzinha posso falar mau dele. Obrigada!')
+print('Gabe eu amo muito ele"')
+print('Gabe é meu amorzinho')
